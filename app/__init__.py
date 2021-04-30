17eff5711238215743c5f777748b64d4662f153c
Kaylyn Cardella <kaylyncardella@gmail.com>
2021-04-30 02:16:00 -0700

    adds book.py, updates __init__.py, runs first migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,27 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate 
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    #hide a warning about a feature in SQLAlchemy that we won't be using
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    #set app.config['SQLALCHEMY_DATABASE_URI'] to the connection string for our database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    #from app folder.models subfolder.book.py
+    #import class Book
+    from app.models.book import Book
+
     return app
+
+
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,16 @@
 from flask import Blueprint
 
+hello_world_bp = Blueprint("hello_world", __name__)
+
+@hello_world_bp.route("/hello-world", methods=["GET"])
+def say_hello_world():
+    response_body = "Hello, Kaylyn!"
+    return response_body
+
+@hello_world_bp.route("/hello-world-json", methods=["GET"])
+def say_hello_json():
+    return {
+        "name": "MEEEEE",
+        "message": "sup",
+        "hobbies": ["Eating bread", "Killing plants", "Watching Bake-Off"]
+    }
